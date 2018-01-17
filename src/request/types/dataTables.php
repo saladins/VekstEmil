00ec39edf3fc26678bc74dfd3611d9c6fe555dac
khest <daklib@gmail.com>
2018-01-17 16:49:10 +0100

    DataTables now includes last updated time and update interval

--- src/request/types/dataTables.php
+++ src/request/types/dataTables.php
@@ -12,7 +12,7 @@ class DataTables {
     }
 
     public function getDataTables() {
-        $sql = "select distinct tableName, providerCode, statisticName from Variable where providerCode regexp '^[0-9]+$' group by tableName;";
+        $sql = "select distinct tableName, providerCode, lastUpdatedDate, updateInterval from Variable where providerCode regexp '^[0-9]+$' group by tableName;";
         $this->db->prepare($sql);
         return $this->db->getResultSet();
     }
