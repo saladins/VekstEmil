8aae023cd45cb5310d2f87e4d52f89773b45ea41
khest <daklib@gmail.com>
2018-01-31 16:57:52 +0100

    Search now returns all variables instead of searching by value

--- src/request/types/search.php
+++ src/request/types/search.php
@@ -18,20 +18,10 @@ class Search {
 
     public function searchTerm($searchTerm) {
         $this->logger->log($searchTerm);
-        $sql = "SELECT variableID, statisticName FROM Variable WHERE statisticName LIKE concat('%', :searchTerm, '%') ORDER BY statisticName";
+        $sql = "SELECT variableID, statisticName FROM Variable ORDER BY statisticName";
         $this->db->prepare($sql);
         $this->db->bind(':searchTerm', $searchTerm);
         $res1 = $this->db->getResultSet();
         return $res1;
-//        $sql = <<<SQL
-//SELECT Variable.variableID, statisticName FROM Variable, VariableTagList, VariableTag
-//WHERE Variable.variableID = VariableTagList.variableID
-//AND VariableTagList.tagID = VariableTag.tagID
-//AND VariableTag.tagText LIKE CONCAT('%', :searchTerm, '%');
-//SQL;
-//        $this->db->prepare($sql);
-//        $this->db->bind(':searchTerm', $searchTerm);
-//        $res2 = $this->db->getResultSet();
-//        return array_merge($res1, $res2);
     }
 }
